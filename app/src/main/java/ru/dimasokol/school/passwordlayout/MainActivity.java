5faeccae56926bec3da93955a8078984818ed6ad
Marija <mzhilkina@bk.ru>
2019-11-19 16:59:12 +0300

    first commit

--- app/src/main/java/ru/dimasokol/school/passwordlayout/MainActivity.java
+++ app/src/main/java/ru/dimasokol/school/passwordlayout/MainActivity.java
@@ -8,9 +8,11 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,11 +23,15 @@ public class MainActivity extends Activity {
     private View mCopyButton;
     private ImageView mQuality;
     private TextView mQualityTextView;
-
     private CompoundButton mUseUppercase;
-
+    private CompoundButton mUseDigits;
+    private CompoundButton mUseChars;
+    private SeekBar mLevelBar;
     private PasswordsHelper mHelper;
-
+    private TextView mLengthView;
+    private Button mGenerateButton;
+    private TextView mTextGen;
+    private View mCopyGenButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +47,12 @@ public class MainActivity extends Activity {
         mCopyButton = findViewById(R.id.button_copy);
         mQuality = findViewById(R.id.quality);
         mQualityTextView = findViewById(R.id.quality_text);
+
+        //Checkboxes
+
         mUseUppercase = findViewById(R.id.check_uppercase);
+        mUseDigits=findViewById(R.id.check_digits);
+        mUseChars=findViewById(R.id.check_char);
 
         mCopyButton.setEnabled(false);
 
@@ -69,14 +80,65 @@ public class MainActivity extends Activity {
                 mResultTextView.setText(mHelper.convert(s));
                 mCopyButton.setEnabled(s.length() > 0);
                 int quality = mHelper.getQuality(s);
-                mQuality.setImageLevel(quality * 1000);
+                mQuality.setImageLevel(quality*2000);
                 mQualityTextView.setText(getResources().getStringArray(R.array.qualities)[quality]);
             }
 
             @Override
-            public void afterTextChanged(Editable s) {
+            public void afterTextChanged(Editable s)
+            {
+            }
+        });
+        mTextGen=findViewById(R.id.result_text_gen);
+        mLevelBar = findViewById(R.id.seek_level);
+        mLengthView= findViewById(R.id.length);
+        mLevelBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+//                mDemoDrawableView.setImageLevel(progress);
+                String chars = getResources().getQuantityString(R.plurals.chars,progress ,progress,progress+8,"Длина :");
+//                getResources().getQuantityString(R.plurals.test3, success, success, total, group)
+
+
+//                String chars=getResources().getQuantityString(R.plurals.test3, progress, progress,10 , "5 tests out of 10 ok");
+                String chars_sum = getResources().getQuantityString(R.plurals.chars, 8+progress);
+                mLengthView.setText(chars);
+//                mLengthView.setText("Длина: 8 + ");
+//                mLengthView.setText(charTest);
+//                        + progress+" "+ chars+" = " +(8+progress) +" "+chars_sum);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
+mGenerateButton= findViewById(R.id.button);
+mGenerateButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        mTextGen.setText(mHelper.gen_password(mLevelBar.getProgress()+8,mUseUppercase.isChecked(),mUseDigits.isChecked(),mUseChars.isChecked()));
+    }
+});
+mCopyGenButton=findViewById(R.id.button_copy_gen);
+mCopyGenButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        manager.setPrimaryClip(ClipData.newPlainText(
+                MainActivity.this.getString(R.string.clipboard_title),
+                mResultTextView.getText()
+        ));
+        Toast.makeText(MainActivity.this, R.string.message_copied, Toast.LENGTH_SHORT)
+                .show();
+    }
+});
     }
 }
--- app/src/main/java/ru/dimasokol/school/passwordlayout/PasswordsHelper.java
+++ app/src/main/java/ru/dimasokol/school/passwordlayout/PasswordsHelper.java
@@ -1,5 +1,9 @@
 package ru.dimasokol.school.passwordlayout;
 
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordsHelper {
 
     private final String[] mRussians;
@@ -36,7 +40,36 @@ public class PasswordsHelper {
         return result.toString();
     }
 
-    public int getQuality(CharSequence password) {
-        return Math.min(password.length(), 10);
+    public int getQuality(CharSequence password)
+    {
+        int score = password.length();
+//                Math.min(10,password.length());
+
+if (password.toString().matches("(.*[0-9]+.*)"))
+    score=score+10;
+if (password.toString().matches("(.*[A-Z]+.*)"))
+            score=score+10;
+Pattern p = Pattern.compile("[^A-Za-z0-9]");
+Matcher m = p.matcher(password.toString());
+if (m.find())
+    score=score+10;
+return Math.min(score/10,4);
+    }
+    public String gen_password(int length, Boolean flag_uppercase,Boolean flag_digits,Boolean flag_chars)
+    {  String uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String digits="0123456789";
+        String chars="!@#$%^&*()_+=-±§<>?.,/`'|;:";
+        String dict="abcdefghijklmnopqrstuvwxyz";
+        if (flag_uppercase)
+            dict=dict+uppercase;
+        if (flag_digits)
+            dict=dict+digits;
+        if (flag_chars)
+            dict=dict+chars;
+        StringBuilder s=new StringBuilder();
+        Random random = new Random();
+        for (int i=0;i<length;i++)
+            s.append (dict.charAt(random.nextInt(dict.length())));
+        return(s.toString());
     }
 }
